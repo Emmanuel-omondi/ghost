00e928a77668c88bf1c93170bdea399eda4c7031
Emmanuel Omondi <emmanuel.omondi@ghostmonitor.com>
2026-04-16 22:32:16 +0300

    Use POSTGRES_URL for Supabase connection

--- server/api/_db.js
+++ server/api/_db.js
@@ -5,7 +5,7 @@ let pool;
 function getPool() {
     if (!pool) {
         pool = new Pool({
-            connectionString: process.env.DATABASE_URL,
+            connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
             ssl: { rejectUnauthorized: false }
         });
     }
